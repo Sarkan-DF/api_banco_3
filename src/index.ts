8effd41b8ded2493efeed316a591c8da13974a29
Igor Matos <sarkan.igor@gmail.com>
2023-08-08 15:53:49 -0300

    Refatoração 2

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,10 @@
-import express, { Request, Response } from "express";
-import * as dotenv from "dotenv";
-import { userRoutes } from "./routes/user.routes";
-import cors from "cors";
 import "reflect-metadata";
 import { Database } from "./main/config/database.conection";
-
-dotenv.config();
-
-const app = express();
-app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-  })
-);
-
-app.use("/users", userRoutes());
+import { Server } from "./main/config/express.config";
 
 Database.connect().then(() => {
   console.log("Database is connected!");
 
-  app.listen(process.env.PORT, () => {
-    console.log("Servidor rodando na porta " + process.env.PORT + "!");
-  });
+  const app = Server.create();
+  Server.listen(app);
 });
---
+++ src/main/config/express.config.ts
@@ -0,0 +1,28 @@
+import cors from "cors";
+import express, { Request, Response, Express } from "express";
+import { userRoutes } from "../../routes/user.routes";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export class Server {
+  public static create() {
+    const app = express();
+    app.use(express.json());
+    app.use(
+      cors({
+        origin: "*",
+      })
+    );
+
+    app.use("/users", userRoutes());
+
+    return app;
+  }
+
+  public static listen(app: Express) {
+    app.listen(process.env.PORT, () => {
+      console.log(`Servidor rodando na porta ${process.env.PORT}`);
+    });
+  }
+}
